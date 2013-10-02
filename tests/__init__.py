ab37ca612a76c098d74ad365c30d56d60ffa5fe6
JDBurnZ <joshuadburns@hotmail.com>
2013-10-02 18:56:45 -0400

    Create __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,79 @@
+valid_fields_dict = {
+	'ip': '12.12.12.12',
+	'billing_city': 'Grand Rapids',
+	'billing_state': 'MI',
+	'billing_zip': '49503',
+	'billing_country': 'USA',
+
+	'shipping_address': '300 Monroe Ave.',
+	'shipping_city': 'Grand Rapids',
+	'shipping_state': 'MI',
+	'shipping_zip': '49503',
+	'shipping_country': 'US',
+
+	'user_domain': 'grcity.us',
+	'user_phone': '6164563000',
+	'user_email': 'info@grcity.us',
+	'user_login': 'some-username',
+	'user_password': 'some-password',
+
+	'bin_cardnumber': '4111111111111111',
+	'bin_name': 'Master Card',
+	'bin_phone': '1-800-627-8372', # 1-800-MasterC
+
+	'session_id': '1234567890abcdefg',
+	'session_useragent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0',
+	'session_language': 'en-US,en;q=0.5',
+
+	'transaction_id': '9876543210',
+	'transaction_amount': '1.98',
+	'transaction_currency': 'USD',
+	'transaction_shopid': '1',
+	'transaction_type': 'creditcard',
+
+	'cc_avs': 'Z',
+	'cc_cvv': 'Y',
+
+	'service_type': 'standard',
+	'proxy_ip': '11.11.11.11',
+}
+
+valid_arguments_dict = {
+	'ip': '12.12.12.12',
+	'billing_city': 'Grand Rapids',
+	'billing_state': 'MI',
+	'billing_zip': '49503',
+	'billing_country': 'USA',
+
+	'shipping_address': '300 Monroe Ave.',
+	'shipping_city': 'Grand Rapids',
+	'shipping_state': 'MI',
+	'shipping_zip': '49503',
+	'shipping_country': 'US',
+
+	'user_domain': 'grcity.us',
+	'user_phone': '6164563000',
+	'user_email': 'info@grcity.us',
+	'user_login': 'some-username',
+	'user_password': 'some-password',
+
+	'bin_cardnumber': '4111111111111111',
+	'bin_name': 'Master Card',
+	'bin_phone': '1-800-627-8372', # 1-800-MasterC
+
+	'session_id': '1234567890abcdefg',
+	'session_useragent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0',
+	'session_language': 'en-US,en;q=0.5',
+
+	'transaction_id': '9876543210',
+	'transaction_amount': '1.98',
+	'transaction_currency': 'USD',
+	'transaction_shopid': '1',
+	'transaction_type': 'creditcard',
+
+	'cc_avs': 'Z',
+	'cc_cvv': 'Y',
+
+	'service_type': 'standard',
+	'proxy_ip': '11.11.11.11',
+}
