23e9b08f428d32b807fbfc1d1ac8a0e7c36292b0
JDBurnZ <joshuadburns@hotmail.com>
2013-10-02 18:58:25 -0400

    Create transform.py

---
+++ pyminfraud/transform.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+
+import hashlib
+
+"""           .s`               +`            +:
+.My`           dm-            .hh           .hM.
+ mMN/          oMMo          /NM+          /NMN
+ sMMMh.        .MMMd.      `yMMM.        `yMMMy
+ -MMMMN+.       dMMMN+::::/mMMMd       ./mMMMM+
+  NMMMMMMMmhs+:.oMMMMMMMMMMMMMMo ./oymMMMMMMMM.
+  yMMMMMMMMMMMMM:MMMMMMMMMMMMMM.dMMMMMMMMMMMMm
+  :MMMMMmmMMMMMM+dMMMMm++sMMMMm-MMMMMMNdMMMMMs
+   NMMMMdo///+shyoMMMMM- dMMMMs+hyo///+sNMMMM:
+   yMMMMMMMMMmhs/-MMMMMh/MMMMM-+ydmMMMMMMMMMM`
+   /MMMMMmNMMMMMM+dMMMMMMMMMMN-MMMMMMNmMMMMMh
+   `MMMMMmyssoshmh/MMMMMMMMMMosmhssssymMMMMMo
+    sMMMMMMMMMNdyo:-dMMMMMMN/-+sdNMMMMMMMMMm.
+     oMMMMMMMMMMMMMN/sMMMMh/dMMMMMMMMMMMMMd`
+    -`+MMMMm:/shNMMMMh+NNosMMMMNdysoNMMMMy.:
+    .m./MMMMN-   :MMMMNooNMMMMy   .mMMMMs.m-
+     mN-:NMMMMms/`oMMMMMMMMMMh -omMMMMM+-NN
+     sMN:-NMMMMMMMdNMMMMMMMMNymMMMMMMM/:NMy
+     :MMM/.mMMMMMMMMMMMMMMMMMMMMMMMMN-/MMM+
+	NMMM+`dMMMMMMMMMMMMMMMMMMMMMMm.+MMMM.
+	yMMMMs`hMMMMMMMMMMMMMMMMMMMMd.sMMMMN
+	/MMMMMy`yMMMMMMMMMMMMMMMMMMh`yMMMMMy
+	`MMMMMMh`sMMMMMMMMMMMMMMMMy`hMMMMMM/
+	 dMMMMMMd.+MMMMMMMMMMMMMMo`dMMMMMMM.
+	 oMMMMMMMm./MMMMMMMMMMMM+.mMMMMMMMm
+	 .dMMMMMMMN-:NMMMMMMMMN:.mMMMMMMMMs
+	   `/smMMMMM/-NMMMMMMN--NMMMMMNy/.
+		 .+hNMM+.mMMMMm.:MMMNh+.
+		    `:sdo`hMMd`/Nh+-
+			  `` sy"""
+
+def md5(value):
+	return hashlib.md5(value).hexdigest()
+
+def bin(value):
+	return value[0:6]
