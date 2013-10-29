3d92592544c1f659fd3840ec522aff0c5bb2ca47
JDBurnZ <joshuadburns@hotmail.com>
2013-10-29 17:55:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
 import setuptools
 
 setuptools.setup(
-	name=pyminfraud.__project__,
-	version=pyminfraud.__version__,
-	author=pyminfraud.__author__,
-	author_email=pyminfraud.__email__,
-	license=pyminfraud.__license__,
-
-	name = 'pyminfraud',
-	version = '1.0',
-	description = 'Python library for interfacing with MaxMind\'s minFraud Web Service API.',
-	keywords = 'python maxmind minfraud',
-	packages = setuptools.find_packages(),
-	author = 'Joshua D. Burns',
-	maintainer = 'Joshua D. Burns',
-	author_email = 'joshuadburns@hotmail.com',
-	url = 'https://github.com/JDBurnZ/pyminfraud',
-	packages=['pyminfraud']
+	name = pyminfraud.__project__,
+	version = pyminfraud.__version__,
+	author = pyminfraud.__author__,
+	author_email = pyminfraud.__email__,
+	license = pyminfraud.__license__,
+	description = pyminfraud.__description__,
+	keywords = 'python maxmind minfraud pyminfraud',
+	author = pyminfraud.__author__,
+	maintainer = pyminfraud.__author__,
+	author_email = pyminfraud.__email__,
+	url = pyminfraud.__url__,
+	packages = ['pyminfraud']
 )
