b695e13b7be72e29c9aa4117242cd2f564e15c78
JDBurnZ <joshuadburns@hotmail.com>
2013-10-29 17:59:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 import setuptools
 
 setuptools.setup(
-	name = pyminfraud.__project__,
-	version = pyminfraud.__version__,
-	author = pyminfraud.__author__,
-	author_email = pyminfraud.__email__,
-	license = pyminfraud.__license__,
-	description = pyminfraud.__description__,
-	keywords = 'python maxmind minfraud pyminfraud',
-	author = pyminfraud.__author__,
-	maintainer = pyminfraud.__author__,
-	author_email = pyminfraud.__email__,
-	url = pyminfraud.__url__,
-	packages = ['pyminfraud']
+	author           = pyminfraud.__author__,
+	author_email     = pyminfraud.__email__,
+	description      = pyminfraud.__description__,
+	keywords         = 'python maxmind minfraud pyminfraud',
+	license          = pyminfraud.__license__,
+	maintainer       = pyminfraud.__author__,
+	maintainer_email = pyminfraud.__email__,
+	name             = pyminfraud.__project__,
+	packages         = ['pyminfraud'],
+	url              = pyminfraud.__url__,
+	version          = pyminfraud.__version__,
 )
